faa08fd8eb64de2a2fa8a56f7540b1aa0274372b
Bruce <bruce@haimi.com>
2016-01-19 15:50:09 +0800

    ini log inherit test and etc.

--- config/ini_test.go
+++ config/ini_test.go
@@ -5,17 +5,18 @@ import (
 	"os"
 )
 
-func TestFileLoggerNormal(t *testing.T) {
+func TestIniConfigNormal(t *testing.T) {
 	dir, err := os.Getwd()
 	if err!=nil {
 		t.Error(err)
 	}
-	t.Log("Working dir now:", dir)
 
 	file:=dir+"/config.ini"
+	t.Log("Working dir now:", dir, " Ini file:", file)
 
 	ini:=Ini(file)
-	sec, err:=ini.GetSection("")
+
+	sec, err:=ini.GetSection("production")
 	if err!=nil {
 		t.Error(err)
 	}
@@ -23,11 +24,34 @@ func TestFileLoggerNormal(t *testing.T) {
 	key, err:=sec.GetKey("log.path")
 	if err!=nil {
 		t.Error(err)
+	}else{
+		if key.String()!="/Users/bruce/project/godev/src/zooinit/log" {
+			t.Error("Get log.path error:", key.String())
+		}else{
+			t.Log("Get log.path:", key.String())
+		}
 	}
+
+	exist:=sec.HasKey("log")
+	if exist {
+		t.Error("Top section should not exist.")
+	}
+
+	secTest:=ini.Section("production.testing")
+
+	t.Log("Get log.ttl:", secTest.Key("log.ttl").String())
+	t.Log("Get log.path:", ini.Section("production.testing").Key("log.path").String())
+
+	for key, value := range secTest.KeysHash() {
+		t.Log(key, value)
+	}
+
 	if key.String()!="/Users/bruce/project/godev/src/zooinit/log" {
-		t.Error("Get log.path error:", key.String())
+		t.Error("Get log.path error for child testing:", key.String())
+	}else{
+		t.Log("Get log.path:", secTest.Key("log.path").String())
 	}
-	t.Log("Get log.path:", key.String())
+
 }
 
 
