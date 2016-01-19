aa4dd8e04a50649c1a1ffcdfe70efafecc0858bc
Bruce <bruce@haimi.com>
2016-01-19 15:51:36 +0800

    ini log inherit test and etc.

--- config/ini_test.go
+++ config/ini_test.go
@@ -7,37 +7,37 @@ import (
 
 func TestIniConfigNormal(t *testing.T) {
 	dir, err := os.Getwd()
-	if err!=nil {
+	if err != nil {
 		t.Error(err)
 	}
 
-	file:=dir+"/config.ini"
+	file := dir + "/config_for_test.ini"
 	t.Log("Working dir now:", dir, " Ini file:", file)
 
-	ini:=Ini(file)
+	ini := Ini(file)
 
-	sec, err:=ini.GetSection("production")
-	if err!=nil {
+	sec, err := ini.GetSection("production")
+	if err != nil {
 		t.Error(err)
 	}
 
-	key, err:=sec.GetKey("log.path")
-	if err!=nil {
+	key, err := sec.GetKey("log.path")
+	if err != nil {
 		t.Error(err)
-	}else{
-		if key.String()!="/Users/bruce/project/godev/src/zooinit/log" {
+	}else {
+		if key.String() != "/Users/bruce/project/godev/src/zooinit/log" {
 			t.Error("Get log.path error:", key.String())
-		}else{
+		}else {
 			t.Log("Get log.path:", key.String())
 		}
 	}
 
-	exist:=sec.HasKey("log")
+	exist := sec.HasKey("log")
 	if exist {
 		t.Error("Top section should not exist.")
 	}
 
-	secTest:=ini.Section("production.testing")
+	secTest := ini.Section("production.testing")
 
 	t.Log("Get log.ttl:", secTest.Key("log.ttl").String())
 	t.Log("Get log.path:", ini.Section("production.testing").Key("log.path").String())
@@ -46,12 +46,17 @@ func TestIniConfigNormal(t *testing.T) {
 		t.Log(key, value)
 	}
 
-	if key.String()!="/Users/bruce/project/godev/src/zooinit/log" {
+	if key.String() != "/Users/bruce/project/godev/src/zooinit/log" {
 		t.Error("Get log.path error for child testing:", key.String())
-	}else{
-		t.Log("Get log.path:", secTest.Key("log.path").String())
+	}else {
+		t.Log("Get log.path for child testing:", secTest.Key("log.path").String())
 	}
 
+	t.Log("Get array:", ini.Section("array").Key("FLOAT64S").Float64s(","))
+	t.Log("Get time:", ini.Section("array").Key("TIMES").Times(","))
+	t.Log("Get string quoted:", ini.Section("comments").Key("key3").Strings(","))
+
+	t.Log([]string{"one", "two", "three"})
 }
 
 
--- config/readme.md
+++ config/readme.md
@@ -1,3 +1,11 @@
 ## To be done
 
     Add zookeeper etc. support with an adapter interface.
+
+## ini格式说明
+
+相关库: github.com/go-ini/ini
+
+    1. 区块继承使用.隔开,例如[production.testing],父类必须写在前面.
+    2. 支持其他变量代换: %(work.dir)s
+    3. 字符串不需要加引号.
