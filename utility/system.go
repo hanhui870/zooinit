7350e22bd8b94a43448de7cc7590187b65b411ae
Bruce <bruce@haimi.com>
2016-01-21 14:43:28 +0800

    String Slice test and cmd parse.

--- utility/system.go
+++ utility/system.go
@@ -3,6 +3,7 @@ package utility
 import (
 	"net"
 	"errors"
+	"strings"
 )
 
 // Fetch ip address of the machine
@@ -91,7 +92,7 @@ func GetLocalIPWithIntranet(findip string) (net.IP, error) {
 		return nil, err
 	}
 
-	ipobj:=net.ParseIP(findip)
+	ipobj := net.ParseIP(findip)
 	for _, i := range ifaces {
 		addrs, err := i.Addrs()
 		if err != nil {
@@ -101,7 +102,7 @@ func GetLocalIPWithIntranet(findip string) (net.IP, error) {
 		for _, addr := range addrs {
 			switch v := addr.(type) {
 			case *net.IPNet:
-				if ipobj.Mask(v.Mask).String()==v.IP.Mask(v.Mask).String() {
+				if ipobj.Mask(v.Mask).String() == v.IP.Mask(v.Mask).String() {
 					return v.IP, nil
 				}
 			}
@@ -109,4 +110,26 @@ func GetLocalIPWithIntranet(findip string) (net.IP, error) {
 	}
 
 	return nil, errors.New("Not found.")
+}
+
+// Need to improve for quoted string
+func ParseCmdStringWithParams(cmd string) (path string, args []string) {
+	delimeter := " \t"
+	if strings.ContainsAny(cmd, delimeter) {
+		// First one is the choise of delimeter
+		for i := 1; i < len(delimeter); i++ {
+			cmd = strings.Replace(cmd, string(delimeter[i]), string(delimeter[0]), -1)
+		}
+		arrSplit := strings.Split(cmd, string(delimeter[0]))
+		path=arrSplit[0]
+
+		for i := 1; i < len(arrSplit); i++ {
+			args=append(args, arrSplit[i])
+		}
+
+		return path, args
+	}else {
+		//only command
+		return cmd, nil
+	}
 }
--- utility/system_test.go
+++ utility/system_test.go
@@ -3,6 +3,8 @@ package utility
 import (
 	"testing"
 	"net"
+	"reflect"
+	"strings"
 )
 
 func TestFetchIPList(t *testing.T) {
@@ -22,25 +24,83 @@ func TestFetchIPList(t *testing.T) {
 	t.Log("Has IP 192.168.4.108: ", HasIpAddress("192.168.4.108"))
 	t.Log("Has IP 127.0.0.1: ", HasIpAddress("127.0.0.1"))
 
-	ip:=net.IPv4(192,168,4,108)
-	t.Log("IP mask of 192.168.4.108: ", ip.Mask(net.IPv4Mask(255,255,255,0)))
+	ip := net.IPv4(192, 168, 4, 108)
+	t.Log("IP mask of 192.168.4.108: ", ip.Mask(net.IPv4Mask(255, 255, 255, 0)))
 	//actual
-	actualMask:=MaskOFIpAddress("192.168.4.108")
+	actualMask := MaskOFIpAddress("192.168.4.108")
 	t.Log("IP mask of 192.168.4.108: ", actualMask.String())
 	t.Log("Actual IP mask of 192.168.4.108: ", ip.Mask(actualMask))
 
 	ip, err = GetLocalIPWithIntranet("192.168.4.199")
-	if err!=nil {
+	if err != nil {
 		t.Error("GetLocalIPWithIntranet of 192.168.4.199:", err)
-	}else{
+	}else {
 		t.Log("Find the smae intranet of 192.168.4.199: ", ip)
 	}
 
 	ip, err = GetLocalIPWithIntranet("192.168.1.4")
-	if err!=nil {
-		t.Error("GetLocalIPWithIntranet of 192.168.1.4:", err)
-	}else{
+	if err != nil {
+		t.Log("GetLocalIPWithIntranet of 192.168.1.4:", err)
+	}else {
 		t.Log("Find the smae intranet of 192.168.1.4: ", ip)
 	}
 
+}
+
+func TestParseCmdStringWithParams(t *testing.T) {
+	type testout struct {
+		path string
+		args []string
+	}
+	type tests struct {
+		in  string
+		out testout
+	}
+
+	testCases := []tests{
+		{in:"start", out:testout{path:"start", args:nil}},
+		{in:"start -name hello", out:testout{path:"start", args:[]string{"-name", "hello"}}},
+		{in:"start -name \"hello\"", out:testout{path:"start", args:[]string{"-name", "\"hello\""}}},
+		{in:"start -name \"hello world\"", out:testout{path:"start", args:[]string{"-name", "\"hello world\""}}},
+	}
+
+	for _, testCase := range testCases {
+		path, args := ParseCmdStringWithParams(testCase.in)
+
+
+		if path != testCase.out.path || !reflect.DeepEqual(args, testCase.out.args) {
+			t.Error("Test failed for:", testCase, " result:", args)
+		}
+	}
+}
+
+
+func TestStringSlice(t *testing.T) {
+	str := "TestParseCmdTestStringWithTestParams"
+
+	var iter int
+	find := "Test"
+	//panic: runtime error: slice bounds out of range [recovered]
+	//t.Log(find[5:])
+	t.Log("String", str, "Slice based AT 5:", str[5:])
+
+	start := 0;
+	for {
+		chunk := str[start:]
+		iter = strings.Index(chunk, find)
+		if iter == -1 {
+			t.Log("Find no", find, "in:", chunk)
+			break
+		} else {
+			t.Log("Find", find, "in:", chunk, "At", iter)
+			//iter是基于当前start的
+			start = start + iter + len(find)
+			if start > len(str) {
+				t.Log("Final break At:", start)
+				break
+			}
+
+			t.Log("Next round start At:", start)
+		}
+	}
 }
