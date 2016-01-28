defd1785969ab4e9a87cde7db91ec674f510421e
Bruce <bruce@haimi.com>
2016-01-28 20:40:29 +0800

    TODO etcd watch test.

--- cluster/etcd/watch_test.go
+++ cluster/etcd/watch_test.go
@@ -5,5 +5,12 @@ import (
 )
 
 func TestWatchTestApi(t *testing.T) {
+	api, err := NewApiKeys([]string{"http://registry.alishui.com:2379"})
+	if err != nil {
+		t.Error("NewApi error:", err)
+	}
 
+	//could waiter a not exist node.
+	w := api.Conn().Watcher("/haimi/test/watcher/not_exist", nil)
+	resp, err := w.Next(Context())
 }
