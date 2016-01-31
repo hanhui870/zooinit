811ba82e5774e94b79d1acb40dcdaeb981bf011b
Bruce <bruce@haimi.com>
2016-01-31 16:02:59 +0800

    Finish etcd watch test.

--- cluster/etcd/watch_test.go
+++ cluster/etcd/watch_test.go
@@ -1,7 +1,9 @@
 package etcd
 
 import (
+	"github.com/coreos/etcd/client"
 	"testing"
+	"time"
 )
 
 func TestWatchTestApi(t *testing.T) {
@@ -10,7 +12,21 @@ func TestWatchTestApi(t *testing.T) {
 		t.Error("NewApi error:", err)
 	}
 
+	go func() {
+		time.Sleep(5 * time.Second)
+
+		api.Conn().CreateInOrder(Context(), "/haimi/test/watcher/not_exist", "this is sequential node", &client.CreateInOrderOptions{})
+	}()
+
 	//could waiter a not exist node.
-	w := api.Conn().Watcher("/haimi/test/watcher/not_exist", nil)
+	w := api.Conn().Watcher("/haimi/test/watcher/not_exist", &client.WatcherOptions{Recursive: true}) //AfterIndex:56,如果有小于现有的,可以马上触发
 	resp, err := w.Next(Context())
+
+	if err != nil {
+		t.Error("Fetch wathch error: ", err)
+	} else {
+		t.Logf("Fetch watch response: %q", resp)
+	}
+
+	time.Sleep(5 * time.Second)
 }
