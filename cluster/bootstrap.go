b9ff4caab25060187283643fcf7d51744dfe5ed7
Bruce <bruce@haimi.com>
2016-02-02 20:49:14 +0800

    // Create qurorum in order node

--- cluster/bootstrap.go
+++ cluster/bootstrap.go
@@ -9,6 +9,7 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/coreos/etcd/client"
 
+	"strconv"
 	"strings"
 	"zooinit/cluster/etcd"
 	"zooinit/config"
@@ -17,6 +18,13 @@ import (
 const (
 	CONFIG_SECTION            = "system.cluster"
 	CLUSTER_BOOTSTRAP_TIMEOUT = 5 * time.Minute
+
+	// 1. consul/config/size qurorum大小
+	// 2. consul/selection/ 候选人选举目录，CreateInOrder
+	// 3. consul/members/ 正式集群中的候选人
+	CLUSTER_CONFIG_DIR    = "/config"
+	CLUSTER_SELECTION_DIR = "/selection"
+	CLUSTER_MEMBER_DIR    = "/members"
 )
 
 var (
@@ -65,7 +73,7 @@ func initializeClusterDiscoveryInfo() {
 	}
 
 	// Set qurorum size
-	resp, err := kvApi.Conn().Set(etcd.Context(), env.discoveryPath, "", &client.SetOptions{Dir: true, TTL: CLUSTER_BOOTSTRAP_TIMEOUT, PrevExist: client.PrevNoExist})
+	resp, err := kvApi.Conn().Set(etcd.Context(), env.discoveryPath, "", &client.SetOptions{Dir: true, TTL: env.timeout, PrevExist: client.PrevNoExist})
 	if err != nil {
 		//ignore exist error
 		if !etcd.EqualEtcdError(err, client.ErrorCodeNodeExist) {
@@ -76,7 +84,24 @@ func initializeClusterDiscoveryInfo() {
 		}
 
 	} else {
-		env.logger.Println("Etcd.Api() set "+env.discoveryPath+" ok:", resp)
+		// Create success, set config
+		env.logger.Println("Etcd.Api() set "+env.discoveryPath+" ok, TTL:", env.timeout.String(), resp)
+
+		// Set config size
+		resp, err = kvApi.Conn().Set(etcd.Context(), env.discoveryPath+CLUSTER_CONFIG_DIR+"/size", strconv.Itoa(env.qurorum), &client.SetOptions{PrevExist: client.PrevNoExist})
+		if err != nil {
+			env.logger.Fatalln("Etcd.Api() set /config/size error:", err)
+		} else {
+			env.logger.Println("Etcd.Api() set /config/size ok, Qurorum size:", env.qurorum, resp)
+		}
+	}
+
+	// Create qurorum in order node
+	resp, err = kvApi.Conn().CreateInOrder(etcd.Context(), env.discoveryPath+CLUSTER_SELECTION_DIR, env.localIP.String(), nil)
+	if err != nil {
+		env.logger.Fatalln("Etcd.Api() CreateInOrder error:", err)
+	} else {
+		env.logger.Println("Etcd.Api() CreateInOrder ok:", resp)
 	}
 
 }
