4541740dbd94873f2942ce25a9edd4724f7674a0
Bruce <bruce@haimi.com>
2016-02-25 17:12:24 +0800

    python signal set

---
+++ script/cluster/signal.py
@@ -0,0 +1,23 @@
+import signal
+import os
+import sys
+import time
+
+
+def signalHandler(signum, frame):
+    print('Signal handler called by:', signum, " will exit now.")
+    sys.exit(1)
+
+
+def registerExitSignal():
+    signal.signal(signal.SIGINT, signalHandler)
+    # signal.signal(signal.SIGKILL, signalHandler) # not support
+    signal.signal(signal.SIGTERM, signalHandler)
+    signal.signal(signal.SIGABRT, signalHandler)
+    signal.signal(signal.SIGSEGV, signalHandler)
+
+
+if __name__ == "__main__":
+    print("Test system signal")
+    registerExitSignal()
+    time.sleep(60)
