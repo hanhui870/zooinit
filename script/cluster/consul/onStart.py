ddd522f9be70b8d3b2849bdf9ba7f33e8adbcd43
Bruce <bruce@haimi.com>
2016-02-26 10:37:05 +0800

    onstart unit test

--- script/cluster/consul/onStart.py
+++ script/cluster/consul/onStart.py
@@ -2,31 +2,19 @@ import subprocess
 import sys
 import io
 from cluster.info import Info
+from subcall import runcmd
 
 
 def run(info):
     if (not isinstance(info, Info)):
         print(__name__ + "::run() info is not instance Info, please check")
+        sys.exit(1)
 
-    try:
-        # fail, no quoted: consul agent -server -data-dir="/tmp/consul" -bootstrap-expect 3  -bind=192.168.4.108 -client=192.168.4.108
-        # If passing a single string, either shell must be True (see below) or else the string must simply name the program to be executed without specifying any arguments.
-        proc = subprocess.Popen(["consul", "agent", "-server",
-                                 "-data-dir=/tmp/consul",
-                                 "-bootstrap-expect", info.Qurorum,
-                                 "-bind=" + info.Localip,
-                                 "-client=" + info.Localip], stdout=subprocess.PIPE)
-
-        for line in iter(proc.stdout.readline, ''):
-            print(line.strip())
-
-            # No need.
-            # proc.wait()
-
-    except subprocess.CalledProcessError as err:
-        print("Found CalledProcessError:", err, err.output)
-    except Exception as err:
-        print("Found error:", err)
+    runcmd.runWithStdoutSync(["consul", "agent", "-server",
+                              "-data-dir=/tmp/consul",
+                              "-bootstrap-expect", info.Qurorum,
+                              "-bind=" + info.Localip,
+                              "-client=" + info.Localip])
 
 
 # ImportError: No module named cluster.utils
