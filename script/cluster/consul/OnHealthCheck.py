61b6db9c3a2ea891c7761d69bffeb0a5468b9601
Bruce <bruce@haimi.com>
2016-03-02 17:34:38 +0800

    python consul client health check finished

--- script/cluster/consul/OnHealthCheck.py
+++ script/cluster/consul/OnHealthCheck.py
@@ -2,16 +2,35 @@ import subprocess
 import sys
 import io
 import time
+import json
+from http.client import HTTPConnection
 from cluster.info import Info
-from subcall import runcmd
+from cluster.consul.Constant import Constant
 
 
+# check cluster member health, un health need exit
 def run(info):
     if (not isinstance(info, Info)):
         print(__name__ + "::run() info is not instance Info, please check")
         sys.exit(1)
 
-    print("Hello world: " + info.Event)
+    url = info.GetServiceUrl(Constant.ClientPort)
+    print("Use endpoint to detect service: " + url)
+    conn = HTTPConnection(url, timeout=Constant.ConnectTimeout)
+    while True:
+        # check leader exists
+        conn.request("get", "/v1/health/node/" + info.GetNodename())
+        resp = conn.getresponse()
+        con = resp.read().decode("UTF-8").strip("")
+        # json need to docode too
+        health = json.loads(con)
+        print("Health info " + info.GetNodename() + ":", resp.status, resp.reason, health)
+        if (len(health) > 0):
+            healthinfo = health[0]
+            if type(healthinfo) == type({}) and "Status" in healthinfo and healthinfo["Status"] == "passing":
+                print("Node " + info.GetNodename() + " health status check passing")
+
+        time.sleep(1)
 
 
 # ImportError: No module named cluster.utils
