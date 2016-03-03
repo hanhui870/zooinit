65fab8a04fca9a33c38c6732ea2950337c018880
Bruce <bruce@haimi.com>
2016-03-03 18:20:26 +0800

    boot pack-cluster-consul first release

--- release/docker/pack-cluster-consul/build.sh
+++ release/docker/pack-cluster-consul/build.sh
@@ -13,13 +13,14 @@ sh -c "./build.sh"
 
 
 #compile go program
-echo -e "Will build go program...\nDir now:" `pwd`
-docker run -v /Users/bruce/:/Users/bruce/ haimi:go-docker-dev bash -c "go build -a -ldflags '-s' zooinit \
+imageBuild="haimi:go-docker-dev"
+echo -e "Will build go program use docker container from image: "$imageBuild"..."
+docker run -v /Users/bruce/:/Users/bruce/ $imageBuild bash -c "go build -a -ldflags '-s' zooinit \
     && mv zooinit /Users/bruce/project/godev/src/zooinit/release/docker/pack-cluster-consul/transfer/bin"
 
 
 #package go program return dir now
-cd -
+cd "../pack-cluster-consul"
 echo -e "Will package go program into docker image...\nDir now:" `pwd`
 
 #package code need no cache, because may change transfer files.
@@ -27,5 +28,5 @@ docker build --no-cache -t haimi:zooinit-cluster-consul .
 
 cd ..
 
-docker tag -f haimi:zooinit registry.alishui.com:5000/haimi:zooinit-cluster-consul
+docker tag -f haimi:zooinit-cluster-consul registry.alishui.com:5000/haimi:zooinit-cluster-consul
 docker push registry.alishui.com:5000/haimi:zooinit-cluster-consul
--- release/docker/pack-cluster-consul/run.sh
+++ release/docker/pack-cluster-consul/run.sh
@@ -5,4 +5,4 @@ set -e
 docker pull registry.alishui.com:5000/haimi:zooinit-cluster-consul
 docker tag -f registry.alishui.com:5000/haimi:zooinit-cluster-consul haimi:zooinit-cluster-consul
 
-docker run -ti --net=host haimi:zooinit-cluster-consul zooinit boot
+docker run -ti --net=host haimi:zooinit-cluster-consul zooinit cluster consul
