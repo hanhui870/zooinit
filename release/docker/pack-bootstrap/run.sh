826b184ed4bf7f52a0a113fc31df2879726c1a40
Bruce <bruce@haimi.com>
2016-03-03 18:14:05 +0800

    run.sh error mode

--- release/docker/pack-bootstrap/run.sh
+++ release/docker/pack-bootstrap/run.sh
@@ -5,4 +5,4 @@ set -e
 docker pull registry.alishui.com:5000/haimi:zooinit-bootstrap
 docker tag -f registry.alishui.com:5000/haimi:zooinit-bootstrap haimi:zooinit-bootstrap
 
-docker run -ti --net=host haimi:zooinit-bootstrap zooinit boot
+docker run -d --net=host haimi:zooinit-bootstrap zooinit boot
