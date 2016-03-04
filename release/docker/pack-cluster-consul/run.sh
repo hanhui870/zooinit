f919e73732dc4d7f79d5149038ff54d8474df313
Bruce <bruce@haimi.com>
2016-03-04 16:14:49 +0800

    # Use -P can expose ports to outside machine for client access.

--- release/docker/pack-cluster-consul/run.sh
+++ release/docker/pack-cluster-consul/run.sh
@@ -5,4 +5,5 @@ set -e
 docker pull registry.alishui.com:5000/haimi:zooinit-cluster-consul
 docker tag -f registry.alishui.com:5000/haimi:zooinit-cluster-consul haimi:zooinit-cluster-consul
 
-docker run -d haimi:zooinit-cluster-consul zooinit cluster consul
+# Use -P can expose ports to outside machine for client access.
+docker run -d -P haimi:zooinit-cluster-consul zooinit cluster consul
