3a5d3561eae2eada8e87c57e743de4ee4517d5a8
Bruce <bruce@haimi.com>
2016-03-07 15:34:04 +0800

    OnHealthCheck: No need loop, Zooinit will handle it, check the exit status.

--- script/cluster/consul/OnHealthCheck.py
+++ script/cluster/consul/OnHealthCheck.py
@@ -9,6 +9,7 @@ from cluster.consul.Constant import Constant
 
 
 # check cluster member health, un health need exit
+# No need loop, Zooinit will handle it, check the exit status.
 def run(info):
     if (not isinstance(info, Info)):
         print(__name__ + "::run() info is not instance Info, please check")
@@ -18,35 +19,33 @@ def run(info):
     url = info.GetServiceUrl(Constant.ClientPort)
     print("Use endpoint to detect service: " + url)
 
-    while True:
-        try:
-            # Found error:timed out while health check, continue loop... need to create every time.
-            conn = HTTPConnection(url, timeout=Constant.ConnectTimeout)
-            # check leader exists
-            conn.request("get", "/v1/health/node/" + info.GetNodename())
-            resp = conn.getresponse()
-            con = resp.read().decode("UTF-8").strip("")
+    try:
+        # Found error:timed out while health check, continue loop... need to create every time.
+        conn = HTTPConnection(url, timeout=Constant.ConnectTimeout)
+        # check leader exists
+        conn.request("get", "/v1/health/node/" + info.GetNodename())
+        resp = conn.getresponse()
+        con = resp.read().decode("UTF-8").strip("")
 
-            # json need to docode too
-            # raise ValueError(errmsg("Expecting value", s, err.value)) from None
-            if con:
-                health = json.loads(con)
+        # json need to docode too
+        # raise ValueError(errmsg("Expecting value", s, err.value)) from None
+        if con:
+            health = json.loads(con)
+        else:
+            health = []
+        print("Health info " + info.GetNodename() + ":", resp.status, resp.reason, health)
+        if (len(health) > 0):
+            healthinfo = health[0]
+            if type(healthinfo) == type({}) and "Status" in healthinfo and healthinfo["Status"] == "passing":
+                print("Node " + info.GetNodename() + " health status check passing")
             else:
-                health = []
-            print("Health info " + info.GetNodename() + ":", resp.status, resp.reason, health)
-            if (len(health) > 0):
-                healthinfo = health[0]
-                if type(healthinfo) == type({}) and "Status" in healthinfo and healthinfo["Status"] == "passing":
-                    print("Node " + info.GetNodename() + " health status check passing")
-                else:
-                    print("Node health check failed.")
-            else:
-                print("Node health info empty.")
+                print("Node health check failed.")
+        else:
+            print("Node health info empty.")
 
-        except Exception as err:
-            print("Found error:" + str(err) + " while health check, continue loop...")
+    except Exception as err:
+        print("Found error:" + str(err) + " while health check, continue loop...")
 
-        time.sleep(1)
 
 
 # ImportError: No module named cluster.utils
