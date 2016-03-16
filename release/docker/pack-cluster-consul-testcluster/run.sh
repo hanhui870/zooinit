1b334fdc122d8ee9d569db71cd0936fb130900e3
Bruce <bruce@haimi.com>
2016-03-16 20:50:38 +0800

    add zooinit-cluster-consul-testcluster

---
+++ release/docker/pack-cluster-consul-testcluster/run.sh
@@ -0,0 +1,33 @@
+# ./run.sh -d service
+#   -d with debug, will not push to registry
+#   -b backend
+#!/usr/bin/env bash
+
+set -e
+
+source ../Constant.rc
+
+if [ "$1" = "-d" ]; then
+    Debug="true"
+else :
+    Debug="false"
+fi
+
+if [ "$2" = "" ]; then
+    service="consul"
+else :
+    service=$2
+fi
+
+Cluster="cluster-consul-testcluster"
+
+if [ "$Debug" = "true" ]; then
+    docker run -ti -P haimi:zooinit-${Cluster} zooinit cluster -b consul $service
+
+else :
+    docker pull ${Registry}/haimi:zooinit-${Cluster}
+    docker tag -f ${Registry}/haimi:zooinit-${Cluster} haimi:zooinit-${Cluster}
+
+    # Use -P can expose ports to outside machine for client access.
+    docker run -d -P haimi:zooinit-${Cluster} zooinit cluster -b consul $service
+fi
